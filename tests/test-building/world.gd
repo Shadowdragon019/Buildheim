a23d81f47d8a5eb36c9f837b4a1e02b7bfb1e719
Roxxane <>
2025-01-12 19:54:10 -0600

    Ground can now be edited on mass & there's a preview. Height jumps have been reintroduced.

--- tests/test-building/world.gd
+++ tests/test-building/world.gd
@@ -7,12 +7,11 @@ const world_bottom := 1000
 const point_width := 5
 const points_per_chunk := 400
 const chunk_width := point_width * points_per_chunk
-const ground_move_height := 20
+const ground_move_height := 40
 
 @onready var player: CharacterBody2D = $Player
 @onready var building_preview : Sprite2D = $BuildingPreview # Shader will break if there's more then one instance
 @onready var snapping_area: Area2D = $SnappingArea
-@onready var terraforming_point: Polygon2D = $TerraformingPoint
 var terraforming_preview: Area2D = create_ground_piece(true)
 var object_rotation := 0.0
 var blocking_objects : Array[Node2D] = []
@@ -21,8 +20,22 @@ var snap_objects : Array[WorldObject] = []
 var chunk_heights : Dictionary = {}
 #Dictionary[int, Dictionary[int, float]]
 var new_chunk_heights : Dictionary = {}
-#Dictionary[int, Dictionary[int, float]]
-var terraforming_points : Dictionary = {}
+var ticks_game_existed := 0
+
+
+func offset_point(point: Vector2i, offset: Vector2i) -> Vector2i:
+	point += offset
+
+	@warning_ignore("integer_division")
+	point.x += point.y / points_per_chunk
+	@warning_ignore("integer_division")
+	point.y -= point.y / points_per_chunk * points_per_chunk
+	if point.y < 0:
+		point.x -= 1
+		point.y += points_per_chunk
+	
+	return point
+
 
 func chunk_index_at_x(x: float) -> int: 
 	@warning_ignore("narrowing_conversion")
@@ -44,6 +57,10 @@ func get_height(chunk_index: int, point_index: int) -> float:
 	if new_chunk_heights.has(chunk_index) && new_chunk_heights[chunk_index].values().has(point_index):
 		return new_chunk_heights[chunk_index][point_index]
 	return chunk_heights[chunk_index][point_index]
+
+
+func get_height_vec(indexes: Vector2i) -> float:
+	return get_height(indexes.x, indexes.y)
 
 
 										   #Dictionary[int, float]
@@ -83,7 +100,7 @@ func generate_chunk_data(chunk_index: int):
 		var start_x = chunk_width * chunk_index
 		for i in range(points_per_chunk + 1):
 			var x : float = start_x + i * point_width
-			var y : float = sin(x/100.0) * 250.0
+			var y : float = sin(x/100.0) * 50.0
 			#if sin(x/2500.0) < 0:
 				#y += sin(x/2500.0) * 2500.0
 			
@@ -129,13 +146,15 @@ func update_ground_piece(ground_piece: Node2D, heights: PackedFloat32Array):
 	
 	var grass_points : PackedVector2Array = []
 	grass_points.resize(heights.size() * 2)
-	var ground_points : PackedVector2Array = [Vector2((heights.size() - 1) * point_width, world_bottom), Vector2(0, world_bottom)]
+	var ground_points : PackedVector2Array = [
+		Vector2((heights.size() - 1) * point_width, world_bottom), Vector2(0, world_bottom)
+	]
 	
 	var index := -1
 	for height in heights:
+		index += 1
 		var x : float = index * point_width
 		var y : float = height
-		index += 1
 		grass_points[index] = Vector2(x, y - 5)
 		grass_points[(heights.size()) * 2 - index - 1] = Vector2(x, y + 5)
 		ground_points.append(Vector2(x, height))
@@ -154,12 +173,11 @@ func load_chunk(chunk_index: int):
 	else:
 		if !chunk_data_exists(chunk_index):
 			generate_chunk_data(chunk_index)
-		#gotta set x offset
 		var piece := create_ground_piece()
 		update_ground_piece(piece, chunk_heights[chunk_index])
 		add_child(piece)
 		piece.name = "GroundCollision" + str(chunk_index)
-		piece.global_position.x = chunk_index * chunk_width + point_width
+		piece.global_position.x = chunk_index * chunk_width
 
 	
 func unload_chunk(chunk_index: int):
@@ -201,24 +219,15 @@ func _ready():
 		load_game()
 	add_child(terraforming_preview)
 	terraforming_preview.modulate = Color(1, 1, 1, 0.5)
-	
-	# Dictionary[int, float]
-	#set_heights(0, {
-		#400: 0,
-		#399: 0,
-		#398: 0,
-		#397: 0,
-		#396: 0,
-	#})
-	#set_heights(1, {
-		#10: 0
-	#})
-	update_ground_piece(terraforming_preview, [-100, -110, -120, -130, -140])
-	terraforming_preview.global_position.x = 100
+	terraforming_preview.top_level = true
 
 
 func _process(_delta: float):
+	ticks_game_existed += 1
 	var global_mouse_position := get_global_mouse_position()
+	@warning_ignore("unused_variable")
+	var global_mouse_global_index := Vector2i(chunk_index_at_x(global_mouse_position.x), point_index_at_x(global_mouse_position.x))
+	
 	# Building
 	#region
 	if Input.is_action_just_pressed("forwards") && TestBuildingGlobal.building_enabled:
@@ -266,16 +275,38 @@ func _process(_delta: float):
 	
 	# Terraforming
 	#region
-	terraforming_point.visible = TestBuildingGlobal.terraforming_enabled
+	terraforming_preview.visible = TestBuildingGlobal.terraforming_enabled
 	if TestBuildingGlobal.terraforming_enabled:
-		var chunk_index := chunk_index_at_x(global_mouse_position.x)
+		var chunk_index := chunk_index_at_x(global_mouse_position.x + point_width / 2.0)
 		var point_index := point_index_at_x(global_mouse_position.x + point_width / 2.0)
-		var current_height := get_height(chunk_index, point_index)
-		var height : float = max(min(global_mouse_position.y, world_bottom - 5, current_height + ground_move_height), current_height - ground_move_height)
-				
-		terraforming_point.global_position = Vector2(point_index * point_width + chunk_index * chunk_width, height)
+		var global_index := Vector2i(chunk_index, point_index)
+		
+		#Dictionary[int, Dictionary[int, float]]
+		var placements := {}
+		var heights : PackedFloat32Array = []
+		var width := 10
+		
+		for index in range(width * 2 + 1):
+			var offset := index - width
+			var offset_global_index := Vector2i(0, offset)
+			var global_index_offset := offset_point(global_index, offset_global_index)
+			var ground_height := get_height_vec(offset_point(global_index, offset_global_index))
+			var weight := absf(width - index) / width
+			weight *= weight
+			weight = 1 - weight
+			var height := minf(lerpf(ground_height, global_mouse_position.y, weight), world_bottom - width)
+			heights.append(height)
+			
+			if !placements.has(global_index_offset.x):
+				placements[global_index_offset.x] = {}
+			placements[global_index_offset.x][global_index_offset.y] = height
+			
+		update_ground_piece(terraforming_preview, heights)
+		terraforming_preview.global_position.x = point_index * point_width + chunk_index * chunk_width - point_width * width
+	
 		if Input.is_action_just_pressed("left_click"):
-			set_heights(chunk_index, {point_index: height})
+			for i in placements:
+				set_heights(i, placements[i])
 	#endregion
 	
 	# Load chunks around player
